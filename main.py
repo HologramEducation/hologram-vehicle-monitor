bc9fce60ca35addbc4f5d320af8fdbfd8b08f1ff
virgilvox <mzara1@asu.edu>
2018-04-18 13:17:27 -0700

    add config file

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import time
 
 import obd
 
-connection = obd.OBD() # auto-connects to USB or RF port
 
+from adafruitConfig import *
 from Adafruit_IO import MQTTClient
+
 from Hologram.HologramCloud import HologramCloud
 
-ADAFRUIT_IO_KEY      = 'YOUR ADAFRUIT IO KEY'
-ADAFRUIT_IO_USERNAME = 'YOUR ADAFRUIT IO USERNAME'
+connection = obd.OBD() # auto-connects to USB or RF port
 
 def connected(client):
     print('Connected to Adafruit IO!')
@@ -36,7 +36,7 @@ while True:
     cmd = obd.commands.SPEED # select an OBD command (sensor)
     response = connection.query(cmd) # send the command, and parse the response
 
-    data = { 'location': location, 'speed': response.value }
+    data = { 'location': str(location), 'speed': response.value }
 
     client.publish('fleet', str(data))
     time.sleep(60)
