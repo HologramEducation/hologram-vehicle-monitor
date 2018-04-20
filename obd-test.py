3c7368ba63d1345e07bbca1996931a34a23284a8
virgilvox <mzara1@asu.edu>
2018-04-19 17:06:35 -0700

    add test

--- obd-test.py
+++ obd-test.py
@@ -2,9 +2,8 @@ import obd
 
 connection = obd.OBD() # auto-connects to USB or RF port
 
-cmd = obd.commands.SPEED # select an OBD command (sensor)
+cmd = obd.commands.STATUS # select an OBD command (sensor)
 
 response = connection.query(cmd) # send the command, and parse the response
 
 print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("mph")) # user-friendly unit conversions
